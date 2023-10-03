93960d7f08ab3c9a51d98462baef9164191e1703
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-10-03 08:11:30 +0000

    [Create] PostImage model

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,29 @@
+class PostImagesController < ApplicationController
+
+  def new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+
+  def index
+    @post_images = PostImage.all
+  end
+
+  def show
+
+  end
+
+  # ストロングパラメータ設定
+  private
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root to: "homes#top"
+
+  devise_for :users
+
+  resources :post_images, only: [:new, :create, :index, :show]
+
+  get "homes/about" => "homes/about", as: "about"
 end
