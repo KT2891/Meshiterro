b04d62eddc4528405b90b8b0d5aca23a2896b265
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-10-04 01:14:34 +0000

    [Add] post_comments

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -18,6 +18,13 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
+  end
+
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to post_images_path
   end
 
   # ストロングパラメータ設定
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-
   root to: "homes#top"
 
   devise_for :users
 
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
+
 
   get "homes/about" => "homes/about", as: "about"
 end
