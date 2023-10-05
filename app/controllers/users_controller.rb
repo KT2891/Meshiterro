ba7438a267db1e37d1150eafea18f29e0930442f
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-10-05 00:11:45 +0000

    [Finish] Meshiterro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -18,5 +20,12 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
